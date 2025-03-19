b0b5dfe1487d6fb4c1934b955b7e1039eb85874d
오연수 <yso7414@naver.com>
2025-03-20 01:19:54 +0900

    feat: User Service 작성
    - 시큐리티 관련 인가를 위함

--- ssfinder-be/src/main/java/com/ssfinder/domain/user/service/UserService.java
+++ ssfinder-be/src/main/java/com/ssfinder/domain/user/service/UserService.java
@@ -1,6 +1,13 @@
 package com.ssfinder.domain.user.service;
 
+import com.ssfinder.domain.user.dto.CustomUserDetails;
+import com.ssfinder.domain.user.entity.User;
+import com.ssfinder.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,7 +25,16 @@ import org.springframework.transaction.annotation.Transactional;
  */
 @Slf4j
 @Service
+@RequiredArgsConstructor
 @Transactional
-public class UserService {
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
 
+    @Override
+    public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
+        User user = userRepository.findById(Integer.valueOf(userId))
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with userId: " + userId));
+
+        return new CustomUserDetails(user.getId());
+    }
 }
