004c9507fc2ae128cdbed214a1af10909273cebc
nature1216 <annekim1216@naver.com>
2025-03-21 09:04:47 +0900

    chore: gitginore

---
+++ ssfinder-be/src/main/java/com/ssfinder/SsfinderBeApplication.java
@@ -0,0 +1,13 @@
+package com.ssfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsfinderBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsfinderBeApplication.class, args);
+	}
+
+}
