659a26c123766d658f861df5fed19828029b2b0e
okeio <dhdustn7414@gmail.com>
2025-03-24 11:37:09 +0900

    feat: FcmToken entity 작성

---
+++ ssfinder-be/src/main/java/com/ssfinder/domain/notification/entity/FcmToken.java
@@ -0,0 +1,55 @@
+package com.ssfinder.domain.notification.entity;
+
+import com.ssfinder.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+/**
+ * packageName    : com.ssfinder.domain.notification.entity<br>
+ * fileName       : *.java<br>
+ * author         : okeio<br>
+ * date           : 2025-03-24<br>
+ * description    :  <br>
+ * ===========================================================<br>
+ * DATE              AUTHOR             NOTE<br>
+ * -----------------------------------------------------------<br>
+ * 2025-03-24          okeio           최초생성<br>
+ * <br>
+ */
+@Entity
+@Table(name = "fcm_token")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class FcmToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "fcm_token", nullable = false)
+    private String fcmToken;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public FcmToken(String fcmToken, User user) {
+        this.fcmToken = fcmToken;
+        this.user = user;
+    }
+}
