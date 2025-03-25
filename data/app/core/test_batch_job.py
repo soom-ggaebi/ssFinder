480cd859c9924a754210d264ad66f0c2c8072aed
이영재 <joker901010@gmail.com>
2025-03-25 22:50:50 +0900

    fix: driver host

--- data/app/core/test_batch_job.py
+++ data/app/core/test_batch_job.py
@@ -11,7 +11,7 @@ def test_batch_job():
             .config("spark.executor.memory", "512m") \
             .config("spark.executor.cores", "1") \
             .config("spark.default.parallelism", "2") \
-            .config("spark.driver.host", "172.31.20.55") \
+            .config("spark.driver.host", "13.209.84.121") \
             .config("spark.driver.port", "4040") \
             .config("spark.driver.bindAddress", "0.0.0.0") \
             .getOrCreate()
