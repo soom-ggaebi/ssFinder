d4644e9c40bd8f74a7e5c05faa4e1404a9a0d147
okeio <dhdustn7414@gmail.com>
2025-03-25 14:45:04 +0900

    feat: 습득 일자 및 상태로 습득물 조회 메서드 추가

--- ssfinder-be/src/main/java/com/ssfinder/domain/found/repository/FoundRepository.java
+++ ssfinder-be/src/main/java/com/ssfinder/domain/found/repository/FoundRepository.java
@@ -1,19 +1,25 @@
 package com.ssfinder.domain.found.repository;
 
 import com.ssfinder.domain.found.entity.FoundItem;
+import com.ssfinder.domain.found.entity.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 /**
  * packageName    : com.ssfinder.domain.found.repository<br>
  * fileName       : FoundRepository.java<br>
  * author         : joker901010<br>
  * date           : 2025-03-19<br>
- * description    :  <br>
+ * description    : Found entity의 repository 클래스입니다. <br>
  * ===========================================================<br>
  * DATE              AUTHOR             NOTE<br>
  * -----------------------------------------------------------<br>
  * 2025-03-19          joker901010           최초생성<br>
+ * 2025-03-25          okeio                 습득일자 및 상태로 조회 메서드 추가<br>
  * <br>
  */
 public interface FoundRepository extends JpaRepository<FoundItem, Integer> {
+    List<FoundItem> findByFoundAtAndStatus(LocalDate date, Status status);
 }
--- ssfinder-be/src/main/java/com/ssfinder/domain/found/service/FoundService.java
+++ ssfinder-be/src/main/java/com/ssfinder/domain/found/service/FoundService.java
@@ -1,8 +1,15 @@
 package com.ssfinder.domain.found.service;
 
-import jakarta.transaction.Transactional;
+import com.ssfinder.domain.found.entity.FoundItem;
+import com.ssfinder.domain.found.entity.Status;
+import com.ssfinder.domain.found.repository.FoundRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
 
 /**
  * packageName    : com.ssfinder.domain.found.service<br>
@@ -14,10 +21,19 @@ import org.springframework.stereotype.Service;
  * DATE              AUTHOR             NOTE<br>
  * -----------------------------------------------------------<br>
  * 2025-03-19          joker901010           최초생성<br>
+ * 2025-03-25          okeio                습득일자 및 상태로 조회 메서드 추가<br>
  * <br>
  */
 @Slf4j
 @Service
+@RequiredArgsConstructor
 @Transactional
 public class FoundService {
+
+    private final FoundRepository foundRepository;
+
+    public List<FoundItem> getStoredItemsFoundDaysAgo(int daysAgo) {
+        return foundRepository.findByFoundAtAndStatus(LocalDate.now().minusDays(daysAgo), Status.STORED);
+    }
+
 }
