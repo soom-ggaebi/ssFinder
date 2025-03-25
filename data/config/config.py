cb4656a0cde28608c03804c93818481db41b994c
이영재 <joker901010@gmail.com>
2025-03-25 17:41:39 +0900

    fix: db port

--- data/config/config.py
+++ data/config/config.py
@@ -25,7 +25,7 @@ SPARK_MASTER = os.getenv("SPARK_MASTER")
 
 # MySQL 설정
 DB_HOST = os.getenv("DB_HOST")
-DB_PORT = int(os.getenv("DB_PORT"))
+DB_PORT = int(os.getenv("DB_PORT",3306))
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
