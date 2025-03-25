4d23b707df76da1ca268bcc7b06afc88d43ca915
nature1216 <annekim1216@naver.com>
2025-03-26 00:57:37 +0900

    feat: UserChatRoom entity 알림 활성 상태 column 추가

--- ssfinder-be/src/main/java/com/ssfinder/domain/chat/entity/UserChatRoom.java
+++ ssfinder-be/src/main/java/com/ssfinder/domain/chat/entity/UserChatRoom.java
@@ -3,6 +3,7 @@ package com.ssfinder.domain.chat.entity;
 import com.ssfinder.domain.user.entity.User;
 import jakarta.persistence.*;
 import lombok.*;
+import org.apache.kafka.common.protocol.types.Field;
 
 import java.time.LocalDateTime;
 
@@ -54,4 +55,8 @@ public class UserChatRoom {
     // mongodb id 값 참조 - 추후 수정 필요하면 수정
     @Column(name = "latest_read_message_id")
     private String latestReadMessageId;
+
+    @Column(name = "notification_enabled", nullable = false)
+    @Builder.Default
+    private Boolean notificationEnabled = true;
 }
