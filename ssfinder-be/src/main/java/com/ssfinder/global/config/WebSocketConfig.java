a1ba16c8dd9ecfdb34d50de620dc984cd58c154e
nature1216 <annekim1216@naver.com>
2025-03-27 15:48:00 +0900

    feat: stomp 엔드포인트 설정 변경
    #14

--- ssfinder-be/src/main/java/com/ssfinder/global/config/WebSocketConfig.java
+++ ssfinder-be/src/main/java/com/ssfinder/global/config/WebSocketConfig.java
@@ -24,8 +24,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(final StompEndpointRegistry registry) {
         registry
                 .addEndpoint("/ws")
-                .setAllowedOrigins("http://127.0.0.1:5500")
-                .withSockJS();
+                .setAllowedOrigins("*");
     }
 
     @Override
