7279414fda48c7957fb7000e68156f2003303ac9
박수미 <psunni@naver.com>
2025-03-28 14:23:03 +0900

    chore: import location_service 삭제

--- ssfinder-fe/lib/services/kakao_login_service.dart
+++ ssfinder-fe/lib/services/kakao_login_service.dart
@@ -7,7 +7,6 @@ import 'dart:async';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import './location_service.dart';
 
 class KakaoLoginService {
   // 싱글톤 패턴 구현
