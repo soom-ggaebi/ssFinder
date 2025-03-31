642ea60139be3b90a38ad6c2c1489263aa7dffb7
okeio <dhdustn7414@gmail.com>
2025-03-31 14:32:35 +0900

    feat: 이동 경로 크로스 체크 요청, 응답 DTO

---
+++ ssfinder-be/src/main/java/com/ssfinder/domain/route/dto/response/RoutesOverlapResponse.java
@@ -0,0 +1,17 @@
+package com.ssfinder.domain.route.dto.response;
+
+/**
+ * packageName    : com.ssfinder.domain.route.dto.response<br>
+ * fileName       : RoutesOverlapResponse.java<br>
+ * author         : okeio<br>
+ * date           : 2025-03-28<br>
+ * description    : 습득자와 분실자 간 이동 경로 크로스 체크에 대한 response DTO 입니다. <br>
+ * ===========================================================<br>
+ * DATE              AUTHOR             NOTE<br>
+ * -----------------------------------------------------------<br>
+ * 2025-03-28          okeio           최초생성<br>
+ * <br>
+ */
+// TODO
+public record RoutesOverlapResponse() {
+}
