8e79564949557a7301e22ff50cfb0ae16fc70c98
okeio <dhdustn7414@gmail.com>
2025-03-31 14:19:16 +0900

    feat: 위치 경로 도메인 서비스

--- ssfinder-be/src/main/java/com/ssfinder/domain/route/service/RouteService.java
+++ ssfinder-be/src/main/java/com/ssfinder/domain/route/service/RouteService.java
@@ -1,23 +1,58 @@
 package com.ssfinder.domain.route.service;
 
-import jakarta.transaction.Transactional;
+import com.ssfinder.domain.route.dto.mapper.UserLocationMapper;
+import com.ssfinder.domain.route.dto.request.RouteCreateRequest;
+import com.ssfinder.domain.route.dto.response.RoutesGetResponse;
+import com.ssfinder.domain.route.dto.response.RoutesOverlapResponse;
+import com.ssfinder.domain.route.entity.UserLocation;
+import com.ssfinder.domain.route.repository.UserLocationRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * packageName    : com.ssfinder.domain.route.service<br>
  * fileName       : RouteService.java<br>
- * author         : joker901010<br>
- * date           : 2025-03-19<br>
- * description    :  <br>
+ * author         : okeio<br>
+ * date           : 2025-03-28<br>
+ * description    : 회원 경로 관련 Service 클래스입니다. <br>
  * ===========================================================<br>
  * DATE              AUTHOR             NOTE<br>
  * -----------------------------------------------------------<br>
- * 2025-03-19          joker901010           최초생성<br>
+ * 2025-03-28          okeio           최초생성<br>
  * <br>
  */
 @Slf4j
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class RouteService {
+    private final UserLocationRepository userLocationRepository;
+    private final UserLocationMapper userLocationMapper;
+
+    public void addRoutes(RouteCreateRequest routeCreateRequest, int userId) {
+        List<UserLocation> userLocations = userLocationMapper.toUserLocations(routeCreateRequest, userId);
+        userLocationRepository.saveAll(userLocations);
+    }
+
+    @Transactional(readOnly = true)
+    public RoutesGetResponse getRoutes(LocalDate date, int userId) {
+        log.info("[경로 조회] date: {}, userId: {}", date, userId);
+        LocalDateTime startOfDay = date.atStartOfDay();
+        LocalDateTime endOfDay = startOfDay.plusDays(1);
+        List<UserLocation> userLocations = userLocationRepository.findByUserIdAndTimestampBetween(userId, startOfDay, endOfDay);
+
+        // TODO 히트맵 (경로 분석 추가)
+        return userLocationMapper.toRoutesGetResponse(userLocations);
+    }
+
+    // TODO
+    public RoutesOverlapResponse checkOverlapRoutes() {
+        return null;
+    }
 }
