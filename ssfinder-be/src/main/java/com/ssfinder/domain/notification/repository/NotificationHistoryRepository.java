d4387c051b94d086803f2bd6d8c58bd8875cac14
okeio <dhdustn7414@gmail.com>
2025-04-01 14:49:45 +0900

    fix: NotificationHistoryRepository 이름 변경

--- ssfinder-be/src/main/java/com/ssfinder/domain/notification/repository/NotificationHistoryRepository.java
+++ ssfinder-be/src/main/java/com/ssfinder/domain/notification/repository/NotificationHistoryRepository.java
@@ -1,6 +1,6 @@
 package com.ssfinder.domain.notification.repository;
 
-import com.ssfinder.domain.notification.entity.Notification;
+import com.ssfinder.domain.notification.entity.NotificationHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
@@ -15,5 +15,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * 2025-03-19          joker901010           최초생성<br>
  * <br>
  */
-public interface NotificationRepository extends JpaRepository<Notification, Integer> {
+public interface NotificationHistoryRepository extends JpaRepository<NotificationHistory, Integer> {
 }
