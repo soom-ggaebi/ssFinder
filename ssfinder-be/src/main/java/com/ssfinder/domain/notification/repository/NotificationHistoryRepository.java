2a9933471442c874881b5f5c6cd19a0c6131d8ce
okeio <dhdustn7414@gmail.com>
2025-04-03 10:59:15 +0900

    fix: 조회 시 삭제된 알림 필터링 하도록 수정

--- ssfinder-be/src/main/java/com/ssfinder/domain/notification/repository/NotificationHistoryRepository.java
+++ ssfinder-be/src/main/java/com/ssfinder/domain/notification/repository/NotificationHistoryRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 /**
  * packageName    : com.ssfinder.domain.notification.repository<br>
@@ -21,8 +22,11 @@ import java.time.LocalDateTime;
  * <br>
  */
 public interface NotificationHistoryRepository extends JpaRepository<NotificationHistory, Integer> {
-    Slice<NotificationHistory> findByUserIdAndTypeOrderBySendAtDesc(Integer userId, NotificationType type, Pageable pageable);
+    // TODO 기존 조회 로직에서 soft delete 체크
+    Slice<NotificationHistory> findByUserIdAndTypeAndIsDeletedFalseOrderBySendAtDesc(Integer userId, NotificationType type, Pageable pageable);
 
-    Slice<NotificationHistory> findByUserIdAndTypeAndSendAtLessThanOrderBySendAtDesc(Integer userId, NotificationType type, LocalDateTime sendAt, Pageable pageable);
+    Slice<NotificationHistory> findByUserIdAndTypeAndIsDeletedFalseAndSendAtLessThanOrderBySendAtDesc(Integer userId, NotificationType type, LocalDateTime sendAt, Pageable pageable);
+
+    Optional<NotificationHistory> findByIdAndUserId(Integer id, Integer userId);
 
 }
