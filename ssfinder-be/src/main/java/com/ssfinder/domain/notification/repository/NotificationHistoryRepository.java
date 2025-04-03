6f8d40f4861015c8b9adfe6f8d485282a928d152
okeio <dhdustn7414@gmail.com>
2025-04-03 21:29:16 +0900

    feat: 알림 타입 필터링하지 않고 전체 알림 이력 조회하는 메서드 추가

--- ssfinder-be/src/main/java/com/ssfinder/domain/notification/repository/NotificationHistoryRepository.java
+++ ssfinder-be/src/main/java/com/ssfinder/domain/notification/repository/NotificationHistoryRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,12 +19,18 @@ import java.util.Optional;
  * DATE              AUTHOR             NOTE<br>
  * -----------------------------------------------------------<br>
  * 2025-04-02          okeio           최초생성<br>
+ * 2025-04-03          okeio           타입 필터링 없는 알림 이력 조회 메서드 추가<br>
  * <br>
  */
 public interface NotificationHistoryRepository extends JpaRepository<NotificationHistory, Integer> {
+
+    Slice<NotificationHistory> findByUserIdAndIsDeletedFalseOrderBySendAtDesc(Integer userId, Pageable pageable);
+
+    Slice<NotificationHistory> findByUserIdAndIdLessThanAndIsDeletedFalseOrderBySendAtDesc(Integer userId, Integer id, Pageable pageable);
+
     Slice<NotificationHistory> findByUserIdAndTypeAndIsDeletedFalseOrderBySendAtDesc(Integer userId, NotificationType type, Pageable pageable);
 
-    Slice<NotificationHistory> findByUserIdAndTypeAndIsDeletedFalseAndSendAtLessThanOrderBySendAtDesc(Integer userId, NotificationType type, LocalDateTime sendAt, Pageable pageable);
+    Slice<NotificationHistory> findByUserIdAndTypeAndIdLessThanAndIsDeletedFalseOrderBySendAtDesc(Integer userId, NotificationType type, Integer id, Pageable pageable);
 
     Optional<NotificationHistory> findByIdAndUserId(Integer id, Integer userId);
 
